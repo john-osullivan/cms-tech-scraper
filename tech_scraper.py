00ab00fbd2ebcc9d12409a7ce36c01ec0ec1d583
John O'Sullivan <johno@mit.edu>
2015-03-13 07:15:03 -0400

    Made the scraper work, just got all the headlines published from 1990 to now into a .csv with headlines, urls, and dates.  Ka-pow!

--- tech_scraper.py
+++ tech_scraper.py
@@ -58,28 +58,54 @@ def build_headline_list():
 
     and returns the list to the user.  Gotta love web scraping.
     '''
+
+    # First, get ALL OF THE LINKS to ALL OF THE ISSUES.
     issue_urls = get_issue_links()
     headline_list = []
+
+    #Now, for each url in the list...
     for issue_url in issue_urls:
+
+        # Start doing everything in a try block -- for some reason BeautifulSoup
+        # fails on Vol 117 Issue N43 and Vol 124 Issue 38.  It seemed easier to
+        # just let the script skip them.
         try:
+
+            # Grab the main div, so we can ignore navigation links... 
             issue_soup = get_soup(issue_url)
             main_div = issue_soup.div(id='main')
+
+            # ... and start going through all of its tags.
             for tags in main_div:
+
+                # Let's grab all the <a>s!
                 main_links = tags.find_all('a')
+                
+                # Then get the publication date and clean it up a little.
                 pub_date = main_links[1].text
                 pub_date = pub_date[pub_date.index(':')+2:]
+
+                # Now, for all of the actual article links...
                 for article_link in main_links[3:]:
                     try:
+
+                        # Add in the (headline, url, date) tuple to our headline list.
                         headline_list.append((article_link.text, issue_url+article_link['href'][0:], pub_date))
                     except KeyError:
                         print "Headline append failed for this tag: " + article_link
         except Exception:
             print "Issue souping failed for this issue URL: " + issue_url
+
+    # Finally, print something pretty for the user and return the list of headlines.
     print "Just finished collecting all of the headlines printed in The Tech from the year 1990 to now."
     print "There were " + str(len(headline_list)) + " headlines found in total."
     return headline_list
 
 if __name__ == '__main__':
+    '''
+    This is what actually runs.  It gets all the headline tuples, 
+    then writes 'em to a .csv. Fun, fun, fun.
+    '''
     headlines = build_headline_list()
 
     with open('headlines.csv', 'wb') as out:
